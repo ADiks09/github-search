a09aa3855e94d1fe216349782ffd3735bd3a1dd5
Anton Andrusenko <andrusenkoanton09@gmail.com>
2024-09-25 12:08:54 +0200

    init project

---
+++ GithubSearch/GithubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchApp.swift
+//  GithubSearch
+//
+//  Created by Anton Andrusenko on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
